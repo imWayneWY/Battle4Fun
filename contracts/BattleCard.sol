7b10a51550150d5903ed820305fd46996c351696
Yan Wei <im.wayne.wy@gmail.com>
2022-11-20 12:45:29 -0800

    change name to battle card

--- contracts/BattleCard.sol
+++ contracts/BattleCard.sol
@@ -4,8 +4,8 @@ pragma solidity ^0.8.4;
 import "erc721a/contracts/ERC721A.sol";
 import "@openzeppelin/contracts/access/Ownable.sol";
 
-contract Battle4Fun is ERC721A, Ownable {
-    constructor() ERC721A("Battle4Fun", "B4F") {}
+contract BattleCard is ERC721A, Ownable {
+    constructor() ERC721A("BattleCard", "BC") {}
 
     function mint(uint256 quantity) external payable {
         // `_mint`'s second argument now takes in a `quantity`, not a `tokenId`.
